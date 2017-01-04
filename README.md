76c6e8459f5bf664437ae167e6d0755b7e55a19a
Tim Dosé <tim.dose@zocdoc.com>
2017-01-04 17:38:32 -0500

    initial import of prototype in progress

---
+++ README.md
@@ -0,0 +1,1 @@
+# bobo-whitelabel-prototype
---
+++ prototype/prototype.js
@@ -0,0 +1,16 @@
+var google = {}
+
+$(window).bind("load", function() {
+	var url = 'http://endless.horse'
+    $('a.appointment-time').attr('href',url)
+
+    $('.book-online-view-profile-buttons').hide();
+
+
+    // Disable click to doctor name & photo
+    $('.profilePhoto a, .docName a').attr('href','');
+    $('.profilePhoto a, .docName a').off('click');
+    $('.profilePhoto a, .docName a').on('click', function() { return false; });
+}
+
+// $('body').css({background:'red'})
