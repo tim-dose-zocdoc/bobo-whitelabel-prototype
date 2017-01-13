8b74d838fd625bf2f38dc0a525762eac76635918
Tim Dosé <tim.dose@zocdoc.com>
2017-01-13 17:03:56 -0500

    index just redirects to axure prototype + moved old index to search

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,10 +1,19 @@
 // var google = {}
 console.log('loading prototype js')
 
+$('.timesViewport').on('change', function() {
+    console.log('.timesViewport changed');
+});
+
 $(window).on("load", function() {
+    console.log('page loaded');
+
+    // Change href of all times grid links to Axure prototype
 	var url = 'http://m9jewt.axshare.com/book.html'
     $('a.appointment-time').attr('href',url)
 
+
+    // Hide booking and profile buttons for each doctor.
     $('.book-online-view-profile-buttons').hide();
 
 
@@ -13,18 +22,19 @@ $(window).on("load", function() {
     $('.profilePhoto a, .docName a').off('click');
     $('.profilePhoto a, .docName a').on('click', function() { return false; });
 
+    // Replace map with image
     console.log('prototype replacing map with image')
     $mapImage = $('<img class="prototype-map" src="prototype/images/map.png">').css({'margin-top':'37px',})
     $('.js-map-over-container').replaceWith($mapImage);
 
+    // Set the initial text of the specialty drop down
     $('#ui-gs-specialty a.ui-gs-toggle').text('Cardiologist (Heart Doctor)')
 
 	console.log('post-load prototype js executed')
 });
 
 
-
-
+// Simplified insurance picker
 $.ajax({
 	url: 'prototype/data/insurances-short.html', 
 	success: function(data) {
